aceaa43ae3c595db0d0dc92eed9dbcd4530f9c4f
Tal-Dahann <taratal8@gmail.com>
2022-12-07 18:23:37 +0200

    Added constants dictionary with colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+
+
+Color? darkBlue = const Color(0xff3d4aa8);
+Color? blue = const Color(0xff4c5dd2);
+Color? lightBlue = const Color(0xff707DD8);
+Color? lightPurple = const Color(0xffa439ca);
+Color? purple = const Color(0xff8d08bd);
+Color? darkPurple = const Color(0xff710697);
+Color? white1 = const Color(0xffffffff);
+Color? lightGray = const Color(0xffAAAAAA);
+Color? darkGray = const Color(0xff707070);
+Color? black = const Color(0xff000000);
