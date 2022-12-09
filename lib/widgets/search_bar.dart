24e425bd8e70b5c2a1897a9d241d2d3e77c7e3f9
danam <danamelamud8@gmail.com>
2022-12-10 01:52:44 +0200

    Updated search_bar.dart

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:paginate_firestore/paginate_firestore.dart';
 import 'package:protestory/firebase/data_provider.dart';
+import 'package:protestory/widgets/text_fields.dart';
 import 'package:provider/provider.dart';
 
 import '../firebase/protest.dart';
@@ -13,8 +14,47 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
+  final _searchController = TextEditingController();
+  String text = 'zzzzz';
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          CustomTextFormField(
+            controller: _searchController,
+            hintText: "Search...",
+            onChanged: (searchText) => setState(() {
+              //print("changed");
+              text = searchText;
+            }),
+            onFieldSubmitted: (searchText) => setState(() {
+              text = searchText;
+            }),
+          ),
+          Container(height: 500, child: Paginator(searchText: text))
+        ],
+      ),
+    );
+    //onFieldSubmitted: ;
+  }
+}
+
+class Paginator extends StatelessWidget {
+  Paginator({required String this.searchText, Key? key}) : super(key: key);
+  String searchText;
+  @override
+  Widget build(BuildContext context) {
+    print("building pagintor: " + searchText);
     return PaginateFirestore(
 //item builder type is compulsory.
       itemBuilder: (context, documentSnapshots, index) {
@@ -30,12 +70,12 @@ class _SearchBarState extends State<SearchBar> {
           .read<DataProvider>()
           .getProtestCollectionRef
           .orderBy('name', descending: false)
-          .where('name', isGreaterThanOrEqualTo: "love")
-          .where('name', isLessThan: "love" + 'z'),
+          .where('name', isGreaterThanOrEqualTo: searchText)
+          .where('name', isLessThan: '${searchText}z'),
 //Change types accordingly
       itemBuilderType: PaginateBuilderType.listView,
 // to fetch real-time data
-      isLive: true,
+      //isLive: true,
     );
   }
 }
