ef1309b998731bf394945a68c99ccdd1dd5948ab
danam <danamelamud8@gmail.com>
2022-12-11 16:17:36 +0200

    Added sorting results by filters in search

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:protestory/utils/add_spaces.dart';
 import 'package:protestory/widgets/text_fields.dart';
 import 'package:provider/provider.dart';
 
 import '../constants/colors.dart';
+import '../constants/tags.dart';
 import '../firebase/data_provider.dart';
+import '../firebase/protest.dart';
 import '../widgets/paginator.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+  final initDropDownValue;
+  const SearchScreen({this.initDropDownValue = 'All', Key? key})
+      : super(key: key);
 
   @override
   State<SearchScreen> createState() => _SearchScreenState();
@@ -17,8 +22,43 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   String text = 'zzzzzzzzzzzzzzzzzzzzzzzzz';
 
+  late String dropDownValue = widget.initDropDownValue;
+
   @override
   Widget build(BuildContext context) {
+    Query<Protest> qry;
+
+    switch (dropDownValue) {
+      case 'Most Recent':
+        {
+          qry = context
+              .read<DataProvider>()
+              .getProtestCollectionRef
+              .orderBy('creation_time', descending: true)
+              .where('prefixes_name', arrayContains: text);
+        }
+        break;
+      case 'Most Popular':
+        {
+          qry = context
+              .read<DataProvider>()
+              .getProtestCollectionRef
+              .orderBy('participants_amount', descending: true)
+              .where('prefixes_name', arrayContains: text);
+        }
+        break;
+      default:
+        {
+          //ALl- sorted by name
+          qry = context
+              .read<DataProvider>()
+              .getProtestCollectionRef
+              .orderBy('name', descending: false)
+              .where('name', isGreaterThanOrEqualTo: text)
+              .where('name', isLessThan: '${text}z');
+        }
+        break;
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search',
@@ -33,22 +73,34 @@ class _SearchScreenState extends State<SearchScreen> {
             icon: const Icon(Icons.search),
             hintText: "Search...",
             onChanged: (searchText) => setState(() {
-              text = searchText;
+              text = searchText.toLowerCase();
             }),
             onFieldSubmitted: (searchText) => setState(() {
-              text = searchText;
+              text = searchText.toLowerCase();
             }),
           ),
           addVerticalSpace(height: 4),
-          Expanded(
-              child: Paginator(
-            query: context
-                .read<DataProvider>()
-                .getProtestCollectionRef
-                .orderBy('name', descending: false)
-                .where('name', isGreaterThanOrEqualTo: text)
-                .where('name', isLessThan: '${text}z'),
-          ))
+          DropdownButton<String>(
+            // Initial Value
+            value: dropDownValue,
+            elevation: 10,
+            icon: const Icon(Icons.keyboard_arrow_down),
+            items: searchFilters.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            // After selecting the desired option,it will
+            // change button value to selected value
+            onChanged: (String? newValue) {
+              setState(() {
+                //TODO: when it is null??
+                dropDownValue = newValue!;
+              });
+            },
+          ),
+          Expanded(child: Paginator(query: qry))
         ],
       ),
     );
