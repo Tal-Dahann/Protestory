3ccced6180466f47e70708d4654458da566f548f
Whitomtit <whitomtit@gmail.com>
2022-12-13 00:24:55 +0200

    Main screen filled with categories

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:protestory/screens/search_screen.dart';
 
 import '../constants/colors.dart';
 import '../widgets/navigation.dart';
+import '../widgets/protest_list_home.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -14,6 +16,11 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    List<SearchOptions> list = [
+      SearchOptions.mostPopular,
+      SearchOptions.mostRecent,
+      SearchOptions.all
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -32,6 +39,17 @@ class _MainScreenState extends State<MainScreen> {
         ),
         backgroundColor: white,
       ),
+      body: ListView.separated(
+          padding: const EdgeInsets.only(bottom: 20.0, top: 10.0),
+          itemBuilder: (context, index) =>
+              ProtestListHome(searchOption: list[index]),
+          separatorBuilder: (context, index) => const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+          itemCount: list.length),
     );
   }
 }
