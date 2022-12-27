8adaeef4522e8c39d7b841eb90be485dee49d553
danam <danamelamud8@gmail.com>
2022-12-27 03:44:15 +0200

    Added Search By tags, updating search screen UI to switch between search and tags, Closed #35

--- lib/providers/search_provider.dart
+++ lib/providers/search_provider.dart
@@ -4,10 +4,28 @@ import '../screens/search_screen.dart';
 
 class SearchPresetsProvider extends ChangeNotifier {
   SearchOptions _searchOption = SearchOptions.all;
+  List<String> _selectedTagsList = [];
+
   SearchPresetsProvider();
 
   SearchOptions get searchOption {
     return _searchOption;
+  }
+
+  List<String> get selectedTagsList {
+    return _selectedTagsList;
+  }
+
+  void addTag(String tag) {
+    _selectedTagsList.add(tag);
+    print("added tag ${tag}");
+    notifyListeners();
+  }
+
+  void removeTag(String tag) {
+    _selectedTagsList.remove(tag);
+    //print("added tag ${tag}");
+    notifyListeners();
   }
 
   set searchOption(SearchOptions searchOptions) {
